fcd4abbfa4c76ad7f28f1cca78eb27f0f9d888b8
Juampykk <juanpablofrias20@gmail.com>
2026-04-18 20:06:03 -0300

    Funcion alta de vehiculo agregada

--- src/main/java/views/Controlador.java
+++ src/main/java/views/Controlador.java
@@ -31,4 +31,33 @@ public class Controlador {
         }
         return new double[] {consumoElectricos, consumoCombustible};
     }
+    
+    
+    public static boolean registrarVehiculoCombustible(String patente, String marcaStr, String modelo,String pais, int anio, double capCarga, int sucursalIdx, double kmPorLitro, double litrosExtra) {
+        if (Persistencia.getVehiculo(patente).isPresent()) {
+            return false; // Si la patente ya existe
+        }
+        
+        domain.Marca marca = new domain.Marca(marcaStr, pais);
+        domain.Sucursal sucursal = Persistencia.getSucursales().get(sucursalIdx);
+        
+        domain.VehiculoCombustible v = new domain.VehiculoCombustible(patente, marca, modelo, anio, capCarga, sucursal, kmPorLitro, litrosExtra);
+        return Persistencia.registrarVehiculo(v);
+    }
+
+    public static boolean registrarVehiculoElectrico(String patente, String marcaStr, String modelo,String pais, int anio, double capCarga, int sucursalIdx, double kwhBase) {
+        if (Persistencia.getVehiculo(patente).isPresent()) {
+            return false; // La patente ya existe
+        }
+        domain.Marca marca = new domain.Marca(marcaStr, pais);
+        domain.Sucursal sucursal = Persistencia.getSucursales().get(sucursalIdx);
+        
+        domain.VehiculoElectrico v = new domain.VehiculoElectrico(patente, marca, modelo, anio, capCarga, sucursal, kwhBase);
+        return Persistencia.registrarVehiculo(v);
+    }
+
+    public static void eliminarVehiculo(String patente) {
+        Optional<Vehiculo> v = Persistencia.getVehiculo(patente);
+        v.ifPresent(vehiculo -> Persistencia.eliminarVehiculo(vehiculo));
+    }
 }
